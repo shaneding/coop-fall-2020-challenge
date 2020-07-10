f60405fa52cc8e3b805f026b4dd44f4a9e5716f7
Shane Ding <s44ding@uwaterloo.ca>
2020-07-10 14:36:40 -0400

    Solution for challenge

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,70 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.historical_val = []
+        # two stacks to keep track of undo/redos
+        self.undo_ = []
+        self.redo_ = []
 
     def add(self, num: int):
-        pass
+        self.historical_val.append(("Add", num, self.value))
+        self.undo_.append(num)
+        self.value += num
 
     def subtract(self, num: int):
-        pass
+        self.historical_val.append(("Subtract", num, self.value))
+        self.undo_.append(num * -1)
+        self.value -= num
 
     def undo(self):
-        pass
+        if self.undo_:
+            operation = self.undo_.pop()
+            if operation >= 0:
+                self.historical_val.append(("Undo add", operation, self.value))
+            else:
+                self.historical_val.append(("Undo subtract", operation, self.value))
+            self.redo_.append(operation)
+            # we use -= as we are technically undoing the operation
+            self.value -= operation
 
     def redo(self):
-        pass
+        if self.redo_:
+            operation = self.redo_.pop()
+            if operation >= 0:
+                self.historical_val.append(("Redo add", operation, self.value))
+            else:
+                self.historical_val.append(("Redo subtract", operation, self.value))
+            self.value += operation
+            self.undo_.append(operation)
 
     def bulk_undo(self, steps: int):
-        pass
+        for i in range(steps):
+            self.undo()
 
     def bulk_redo(self, steps: int):
-        pass
+        for i in range(steps):
+            self.redo()
+
+    # function for debugging/printing out historical changes
+    def print_history(self):
+        for operation in self.historical_val:
+            if "add" in operation[0]:
+                connector = "to"
+            else:
+                connector = "from"
+            
+            print(operation[0], operation[1], connector, operation[2])
+        
+        print("Current/Final value is", self.value)
+
+
+sourcer = EventSourcer()
+sourcer.add(10)
+sourcer.subtract(5)
+sourcer.add(15)
+sourcer.add(5)
+sourcer.undo()
+sourcer.undo()
+sourcer.redo()
+sourcer.bulk_undo(5)
+sourcer.print_history()
